43d33daa228be718459e85c8e0e219a7f9a11e30
Sergei Silnov <po@kumekay.com>
2021-09-08 22:15:04 +0200

    Combine js assets into single html

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,53 +1,59 @@
-const path = require('path');
-const production = process.env.NODE_ENV === 'production';
-const TerserPlugin = require('terser-webpack-plugin');
+const path = require("path");
+const production = process.env.NODE_ENV === "production";
+const TerserPlugin = require("terser-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlInlineScriptPlugin = require("html-inline-script-webpack-plugin");
 
 module.exports = {
-	mode: production ? 'production' : 'development',
-	devtool: 'inline-source-map',
-	entry: './src/main.tsx',
-	output: {
-		path: path.join(__dirname, 'dist'),
-		filename: 'bundle.js'
-	},
-	resolve: {
-		extensions: ['.ts', '.tsx', '.js', '.jsx']
-	},
-	optimization: {
-		minimizer: [
-			new TerserPlugin({
-				parallel: true,
-				terserOptions: {
-					ecma: 6
-				}
-			})
-		]
-	},
-	module: {
-		rules: [
-			{
-				test: /\.css$/,
-				use: ['style-loader', 'css-loader']
-			},
-			{
-				test: /\.jsx?$/,
-				exclude: /node_modules/,
-				use: ['babel-loader']
-			},
-			{
-				test: /\.tsx?$/,
-				loader: 'ts-loader',
-				options: {
-					transpileOnly: true
-				}
-			}
-		]
-	},
+  mode: production ? "production" : "development",
+  devtool: "inline-source-map",
+  entry: "./src/main.tsx",
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ title: "Minored UI", template: "index.html" }),
+    new HtmlInlineScriptPlugin(),
+  ],
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
+  },
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        parallel: true,
+        terserOptions: {
+          ecma: 6,
+        },
+      }),
+    ],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+        options: {
+          transpileOnly: true,
+        },
+      },
+    ],
+  },
 
-	devServer: {
-		host: '0.0.0.0',
-		compress: true,
-		disableHostCheck: true,
-		overlay: true
-	}
+  devServer: {
+    host: "0.0.0.0",
+    compress: true,
+    disableHostCheck: true,
+    overlay: true,
+  },
 };
